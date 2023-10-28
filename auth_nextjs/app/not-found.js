f21dee7c1d41e56952ff2761d2640a71e86199cb
albertotomatis <hellohako26@gmail.com>
2023-10-28 19:53:45 +0200

    404 error page

---
+++ auth_nextjs/app/not-found.js
@@ -0,0 +1,10 @@
+export default function NotFound() {
+    return (
+    <div class="flex h-screen flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h1 class="text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Page not found</h1>
+        <h2 class="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Error 404</h2>
+      </div>
+    </div>
+      )
+  }
