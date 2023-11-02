0ad9fad60d0d25a1e54f9497d39ddd5fbdd8cc78
albertotomatis <hellohako26@gmail.com>
2023-11-02 08:10:32 +0100

    parsing dell'ID

--- auth_nextjs/app/api/listaPost/[id]/route.js
+++ auth_nextjs/app/api/listaPost/[id]/route.js
@@ -3,14 +3,14 @@ import Post from "@/models/post";
 import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  // Estrai i parametri dalla URL utilizzando un'espressione regolare
+  // Estrae i parametri dalla URL utilizzando un'espressione regolare
   const match = req.url.match(/listaPost\/(\w+)/);
 
   if (!match) {
     return NextResponse.json({ message: "Error" }, { status: 404 });
   }
 
-  const id = match[1]; // Estrai l'ID dall'espressione regolare
+  const id = match[1]; // Estrae l'ID dall'espressione regolare
 
   await connectMongoDB();
   const post = await Post.findOne({ _id: id });
