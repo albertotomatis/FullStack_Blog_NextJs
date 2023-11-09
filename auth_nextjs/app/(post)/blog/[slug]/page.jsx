08cf919e4ba20aa15241142995b78a645b38e17c
albertotomatis <hellohako26@gmail.com>
2023-11-09 21:57:08 +0100

    layout post singolo

--- auth_nextjs/app/(post)/blog/[slug]/page.jsx
+++ auth_nextjs/app/(post)/blog/[slug]/page.jsx
@@ -1,3 +1,10 @@
+import Link from "next/link";
+import { BiEdit } from "react-icons/bi";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import RemoveBtn from "@/app/components/Posts/RemoveBtn";
+import { ImPacman } from "react-icons/im";
+
 const getPostBySlug = async (slug) => {
   try {
     const res = await fetch(`http://localhost:3000/api/listaPost/${slug}`, {
@@ -13,15 +20,49 @@ const getPostBySlug = async (slug) => {
   }
 };
 
+
 export default async function SinglePost({ params }) {
   const { slug } = params;
   const { post } = await getPostBySlug(slug);
+  const session = await getServerSession(authOptions);
+  {/* Formattazione della data (ad esempio: 6 novembre 2023 */}
+  const formatItalianDate = (date) => {
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return new Intl.DateTimeFormat('it-IT', options).format(new Date(date));
+  }
 
   return (
-    <div className="flex h-screen flex-col justify-center px-6 py-12 lg:px-8">
-      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-        <h1>{post.title}</h1>
-        <p>{post.content}</p>
+    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8 pt-16">
+      <article key={post._id} className="p-6 bg-white rounded-lg border border-[#e0e0e0]">
+            <div className="flex justify-between items-center mb-5 text-gray-600">
+              <span className="bg-[#F5E9C4] text-[#333333] text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded">
+              <ImPacman size={14} className="mr-1" />
+                {post.category}
+              </span>
+              {/* data */}
+              <span className="text-sm">{formatItalianDate(post.createdAt)}</span>
+            </div>
+            {/* title */}
+            <h2 className="mb-2 text-3xl font-bold">{post.title}</h2>
+            <p className="my-5">{post.content}</p>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-4">
+                <span className="font-medium">CodeLab Team</span>
+              </div>
+            </div>
+            <div className="flex gap-3">
+              {session ? (
+                session.user.role === "admin" ||
+                (session.user.role === "author" && session.user.id === post.id) ? (
+                  <Link href={`/editPost/${post._id}`} className="text-[#4285f4] font-medium hover:underline pt-5">
+                    <BiEdit size={24} />
+                  </Link>
+                ) : null
+              ) : null}
+              <RemoveBtn id={post._id} />
+            </div>
+      </article>
       </div>
     </div>
   );
--- auth_nextjs/app/components/Posts/PostList.jsx
+++ auth_nextjs/app/components/Posts/PostList.jsx
@@ -62,9 +62,7 @@ export default async function PostList() {
           <article key={post._id} className="p-6 bg-white rounded-lg border border-[#e0e0e0]">
             <div className="flex justify-between items-center mb-5 text-gray-600">
               <span className="bg-[#F5E9C4] text-[#333333] text-sm font-medium inline-flex items-center px-2.5 py-0.5 rounded">
-                <svg className="mr-1 w-4 h-4" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-                </svg>
+              <ImPacman size={14} className="mr-1" />
                 {post.category}
               </span>
               {/* data */}
