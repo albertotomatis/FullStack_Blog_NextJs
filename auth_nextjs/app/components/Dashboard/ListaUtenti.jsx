345410179f3ab31ab62b6e56c4c293d537873cff
albertotomatis <hellohako26@gmail.com>
2023-11-14 23:04:32 +0100

    paginazione tabella utenti

--- auth_nextjs/app/components/Dashboard/ListaUtenti.jsx
+++ auth_nextjs/app/components/Dashboard/ListaUtenti.jsx
@@ -3,8 +3,7 @@ import Link from "next/link";
 import { useState, useEffect } from 'react';
 import { useSession } from "next-auth/react";
 import { useRouter } from 'next/navigation';
-import DeleteUser from "@/app/components/Dashboard/DeleteUser";
-import { GoPersonAdd } from "react-icons/go";
+import Paginate from "@/app/components/Dashboard/Paginate";
 
 const ListaUtenti = () => {
   const router = useRouter();
@@ -15,7 +14,7 @@ const ListaUtenti = () => {
     try {
       const response = await fetch(`http://localhost:3000/api/listaUtenti`);
       const data = await response.json();
-      setUsers(data.utenti); 
+      setUsers(data.utenti);
       console.log("Fetched Data:", data);
     } catch (error) {
       console.error("Error fetching users:", error);
@@ -26,56 +25,19 @@ const ListaUtenti = () => {
     fetchUsers(); 
   }, []); 
 
+
   return (
     <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8 pt-16">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm py-5">
-        <h1 className="mb-4 mt-8 text-3xl lg:text-3xl font-extrabold">Hello, <span className="text-[#009688]">{session?.user?.name}</span> </h1>
+        <h1 className="mb-4  text-3xl lg:text-3xl font-extrabold">Hello, <span className="text-[#009688]">{session?.user?.name}</span> </h1>
         <button type="button" className="btn font-semibold rounded-lg text-sm px-4 py-2 text-center mr-5">
           <Link href="/creaPost" className="text-white hover:text-white">Crea post</Link>
         </button>
       </div>
-        
-        {session && session.user && session.user.role === "admin" && (
-  <div>
-    <h3 className="mb-4 mt-8 text-3xl lg:text-3xl font-extrabold hover:text-white">Lista Utenti</h3>
-    <table className="border-2-collapse border-2 w-full">
-      <thead>
-        <tr>
-          <th className="border-2-2 p-3" colSpan="4">
-            <span className="float-right">
-            <Link href="/register"
-              className="bg-[#4285f4] text-[#ffffff] text-md lg:text-md font-semibold inline-flex items-center px-3.5 py-1.5 rounded">
-                <GoPersonAdd size={22} className="mr-2" /> Registra un nuovo utente
-              </Link>
-            </span>
-          </th>
-        </tr>
-        <tr>
-          <th className="border-2 p-3">Nome</th>
-          <th className="border-2 p-3">Email</th>
-          <th className="border-2 p-3">Role</th>
-          <th className="border-2 p-3">Azioni</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((user, index) => (
-          <tr key={user._id} className={index % 2 === 0 ? 'bg-green-100' : 'bg-white'}>
-            <td className="border-2 p-3">{user.name}</td>
-            <td className="border-2 p-3">{user.email}</td>
-            <td className="border-2 p-3">{user.role}</td>
-            <td className="border-2 p-3">
-              <DeleteUser id={user._id} />
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  </div>
-)}
-
-      </div>
-    </div>
+      <Paginate users={users} />
+</div>
+</div>
   );
   
 }
---
+++ auth_nextjs/app/components/Dashboard/Paginate.jsx
@@ -0,0 +1,90 @@
+import ReactPaginate from 'react-paginate';
+import { useState } from "react";
+import Link from "next/link";
+import DeleteUser from "@/app/components/Dashboard/DeleteUser";
+import { GoPersonAdd } from "react-icons/go";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import { HiOutlineArrowNarrowLeft } from "react-icons/hi";
+
+const Paginate = ({ users }) => {
+    const itemsPerPage = 3;
+    const [currentPage, setCurrentPage] = useState(0);
+  
+    // Calcola l'offset
+    const offset = currentPage * itemsPerPage;
+  
+    // Calcola l'array degli utenti paginati
+    const paginatedUsers = users && users.slice(offset, offset + itemsPerPage);
+  
+    // Calcola il numero totale di utenti visualizzati
+    const totalDisplayedUsers = offset + (paginatedUsers?.length || 0);
+  
+    const handlePageClick = ({ selected }) => {
+      setCurrentPage(selected);
+    };
+
+  return (
+    <div>
+<h3 className="mb-4 mt-8 text-3xl lg:text-3xl font-extrabold hover:text-white">Lista Utenti</h3>
+<table className="border-2-collapse border-2 w-full">
+      <thead>
+        <tr>
+          <th className="border-2-2 p-3" colSpan="4">
+            <span className="float-right">
+            <Link href="/register"
+              className="bg-[#4285f4] text-[#ffffff] text-md lg:text-md font-semibold inline-flex items-center px-3.5 py-1.5 m-2 rounded">
+                <GoPersonAdd size={22} className="mr-2" /> Registra un nuovo utente
+              </Link>
+            </span>
+          </th>
+        </tr>
+              <tr>
+                <th className="border-2 p-3">Nome</th>
+                <th className="border-2 p-3">Email</th>
+                <th className="border-2 p-3">Role</th>
+                <th className="border-2 p-3">Azioni</th>
+              </tr>
+            </thead>
+            {/* Corpo della tabella */}
+            <tbody>
+              {paginatedUsers && paginatedUsers.map((user, index) => (
+                <tr key={user._id} className={index % 2 === 0 ? 'bg-green-100' : 'bg-white'}>
+                  <td className="border-2 p-3">{user.name}</td>
+                  <td className="border-2 p-3">{user.email}</td>
+                  <td className="border-2 p-3">{user.role}</td>
+                  <td className="border-2 p-3">
+                    <DeleteUser id={user._id} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            {/* Footer */}
+            <tfoot>
+  <tr>
+    <td colSpan="4" className="text-center">
+      <div className="max-w-screen-xl mx-auto p-4 flex items-center justify-between m-2">
+        <span className="mr-2">Utenti {`${totalDisplayedUsers} / ${users.length} `}</span>
+        <div className="flex items-center">
+          <ReactPaginate
+            pageCount={Math.ceil(users.length / itemsPerPage)}
+            onPageChange={handlePageClick}
+            containerClassName="pagination flex space-x-6 items-center" 
+            pageClassName="page-item hidden"
+            pageLinkClassName="page-link"
+            activeClassName="active"
+            previousLabel={<button className="bg-[#009688] text-[#ffffff] text-md lg:text-md font-semibold inline-flex items-center px-3.5 py-1.5 rounded">
+                <span><HiOutlineArrowNarrowLeft size={22} className="mr-2" /></span>Prev</button>}
+            nextLabel={<button className="bg-[#009688] text-[#ffffff] text-md lg:text-md font-semibold inline-flex items-center px-3.5 py-1.5 rounded">
+                Next<span><HiOutlineArrowNarrowRight size={22} className="ml-2" /></span> </button>}
+          />
+        </div>
+      </div>
+    </td>
+  </tr>
+</tfoot>
+          </table>
+          </div>
+  );
+};
+
+export default Paginate;
