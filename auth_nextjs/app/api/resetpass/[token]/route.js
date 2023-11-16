fc573aacce9ec3ef35a750cfe3e532811dd16d98
albertotomatis <hellohako26@gmail.com>
2023-11-16 12:42:49 +0100

    scadenza token

--- auth_nextjs/app/api/resetpass/[token]/route.js
+++ auth_nextjs/app/api/resetpass/[token]/route.js
@@ -5,44 +5,80 @@ import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
 export async function POST(req) {
+  await connectMongoDB();
+
   try {
     const { token, newPassword } = await req.json();
-    await connectMongoDB();
 
     // Verifica se il token è valido nel database
-    const user = await User.findOne({
-      passwordResetToken: token,
-      passwordResetTokenExpiry: { $gt: new Date() },
-    });
+    const decodedToken = jwt.decode(token);
 
-    if (!user) {
-      return NextResponse.json({ message: "TokenExpired" }, { status: 400 });
+    try {
+      // Verifica del token e ottenimento delle informazioni
+      const tokenInfo = jwt.verify(token, process.env.SECRET_KEY_WITH_SALT);
+      const tokenExpirationLocal = new Date(tokenInfo.exp * 1000);
+
+      // Verifica se il token è presente nel database
+      const user = await User.findOne({
+        passwordResetToken: token,
+        passwordResetTokenExpiry: { $gt: tokenExpirationLocal }
+      });
+
+      // Controlla se l'utente non è presente nel database
+      if (!user) {
+        return NextResponse.json({ message: "TokenNotFoundOrExpired" }, { status: 400 });
+      }
+
+      // Controlla se il token fornito nell'URL è diverso da quello nel database
+      if (user.passwordResetToken !== token) {
+        return NextResponse.json({ message: "InvalidToken" }, { status: 400 });
+      }
+
+      // Controlla se il token è scaduto per l'utente nel database
+      if (user.passwordResetTokenExpiry <= new Date()) {
+        return NextResponse.json({ message: "TokenExpiredForUser" }, { status: 401 });
+      }
+
+      // Hash della nuova password
+      const saltRounds = 10;
+      const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
+
+      // Aggiorna la password dell'utente
+      user.password = hashedPassword;
+
+      // Cancella il token di reset
+      user.passwordResetToken = undefined;
+      user.passwordResetTokenExpiry = undefined;
+
+      await user.save();
+
+      // Genera un nuovo token di sessione JWT dopo la reimpostazione della password
+      const newToken = jwt.sign({ userId: user._id }, process.env.SECRET_KEY_WITH_SALT, {
+        expiresIn: '1h', // Scadenza del token 
+      });
+
+      // Invia il nuovo token con la risposta
+      return NextResponse.json({ message: "Password reset successful", token: newToken }, { status: 200 });
+
+    } catch (tokenError) {
+      console.error("Errore durante la verifica del token:", tokenError);
+
+      if (tokenError instanceof jwt.TokenExpiredError) {
+        // Gestisci specificamente l'errore di token scaduto
+        return NextResponse.json({ message: "TokenExpiredError", error: tokenError.message }, { status: 401 });
+      } else if (tokenError instanceof jwt.JsonWebTokenError) {
+        // Gestisci specificamente l'errore di firma del token
+        return NextResponse.json({ message: "InvalidTokenError", error: tokenError.message }, { status: 401 });
+      }
+
+      // Gestisci altri errori di verifica del token
+      return NextResponse.json({ message: "TokenVerificationError", error: tokenError.message }, { status: 401 });
     }
-
-    // Hash della nuova password
-    const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(newPassword, saltRounds);
-
-    // Aggiorna la password dell'utente
-    user.password = hashedPassword;
-
-    // Cancella il token di reset
-    user.passwordResetToken = undefined;
-    user.passwordResetTokenExpiry = undefined;
-
-    await user.save();
-
-    // Genera un nuovo token di sessione JWT dopo la reimpostazione della password
-    const newToken = jwt.sign({ userId: user._id }, process.env.SECRET_KEY_WITH_SALT, {
-      expiresIn: '1h', // Scadenza del token (personalizzala)
-    });
-    
-    // Invia il nuovo token con la risposta
-    return NextResponse.json({ message: "Password reset successful", token: newToken }, { status: 200 });
   } catch (error) {
-    console.error(error);
+    console.error("Errore generale:", error);
+    // Gestisci gli errori al di fuori del blocco try interno
     return NextResponse.json(
-      { message: "An error occurred while resetting the password." },
+      { message: "Si è verificato un errore durante la reimpostazione della password.", error: error.message },
       { status: 500 }
     );
   }
--- auth_nextjs/app/components/Auth/ResetPasswordForm.jsx
+++ auth_nextjs/app/components/Auth/ResetPasswordForm.jsx
@@ -3,30 +3,20 @@ import { useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useRouter } from 'next/navigation';
 import { isValidPassword } from "../../../utils/validation";
+import Link from 'next/link';
 
 const validateFormData = (formData) => {
   const errors = {};
-
   if (!formData.password) {
     errors.password = "La password è un campo obbligatorio";
+  } else if (!isValidPassword(formData.password)) {
+    errors.password = "La password non è valida. Assicurati che abbia almeno 8 caratteri, un carattere maiuscolo, un carattere minuscolo, un numero e un carattere speciale.";
   }
-
   return errors;
-}
-
-const handleInputChange = (event, formData, setFormData, errors, setErrors, isValidEmail) => {
-  const { name, value } = event.target;
-
-  setFormData({
-    ...formData,
-    [name]: value
-  });
 }
 
 export default function ResetPasswordForm() {
   const router = useRouter();
-
-  // Prende il token dall'URL
   const searchParams = useSearchParams();
   const token = searchParams.get('token');
 
@@ -34,15 +24,21 @@ export default function ResetPasswordForm() {
     password: '',
   });
 
-
-
   const [errors, setErrors] = useState({});
   const [tokenExpired, setTokenExpired] = useState(false);
-
-
+  const [errorMessage, setErrorMessage] = useState(null);
 
   const handleChange = (e) => {
-    handleInputChange(e, formData, setFormData, errors, setErrors, isValidPassword);
+    const { name, value } = e.target;
+  
+    // Validazione della password in tempo reale
+    const newErrors = validateFormData({ ...formData, [name]: value });
+    setErrors(newErrors);
+  
+    setFormData({
+      ...formData,
+      [name]: value
+    });
   }
 
   const handleSubmit = async (e) => {
@@ -57,10 +53,6 @@ export default function ResetPasswordForm() {
       return;
     }
 
-    if (tokenExpired) {
-      return;
-    }
-
     try {
       const resetRes = await fetch(`/api/resetpass/${token}`, {
         method: 'POST',
@@ -71,62 +63,76 @@ export default function ResetPasswordForm() {
       });
 
       if (resetRes.ok) {
+        setFormData({ password: '' });
         router.push('/reset-password-success');
       } else {
         const response = await resetRes.json();
-        if (response.error === 'TokenExpired') {
+
+        // Utilizza il messaggio di errore fornito dal server
+        if (response.message === 'TokenExpiredError') {
+          setErrorMessage(
+            <p className="text-red-400 text-center font-bold mb-4">
+              Link per il reset  password scaduto.{" "}
+              <Link href="/forgotPassword" className="text-red-400 underline">
+                Richiedi un nuovo link
+              </Link>
+              .
+            </p>
+          );
           setTokenExpired(true);
+        } else if (response.message === 'InvalidTokenError') {
+          setErrorMessage(
+            <>
+              Link non valido. <br />
+              Utilizza il link ricevuto per email.
+            </>
+          );
         } else {
-          console.error('Errore durante il reset della password.');
+          setErrorMessage(response.message);
         }
       }
     } catch (resetError) {
-      console.error(resetError);
+      // Gestione generica degli errori durante la richiesta
+      setErrorMessage("Si è verificato un errore durante il reset della password.");
     }
-  };
+  }
 
   return (
     <section className="bg-gray-50 h-screen flex items-center justify-center">
       <div className="w-full p-6 bg-white rounded-lg shadow-md:mt-0 sm:max-w-md sm:p-20 shadow-lg">
-        {tokenExpired ? (
-          <div className="text-center">
-            <p>Il link per il reset della password è scaduto.</p>
-            <p>Per favore, richiedi un nuovo link per il reset della password.</p>
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mb-4 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">
+            Nuova password
+          </h2>
+        </div>
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="text-red-400 text-center font-bold mb-4">
+            {errorMessage}
           </div>
-        ) : (
-          <>
-            <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-              <h2 className="mb-4 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">
-                Nuova password
-              </h2>
+          <form onSubmit={handleSubmit} className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#" method="POST">
+            <div>
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 px-2.5">Nuova Password</label>
+              <div className="mt-2">
+                <input
+                  onChange={handleChange}
+                  name="password"
+                  type="password"
+                  className="block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus-1inset focus:ring-sky-900 sm:text-sm sm:leading-6"
+                />
+                {errors.password && (
+                  <p className="text-red-400 font-bold w-fit text-sm py-1 px-3 rounded-md mt-2">
+                    {errors.password}
+                  </p>
+                )}
+              </div>
             </div>
-            <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-              <form onSubmit={handleSubmit} className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#" method="POST">
-                <div>
-                  <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900 px-2.5">Nuova Password</label>
-                  <div className="mt-2">
-                    <input
-                      onChange={handleChange}
-                      name="password"
-                      type="password"
-                      className="block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus-1inset focus:ring-sky-900 sm:text-sm sm:leading-6"
-                    />
-                    {errors.password && (
-                      <p className="text-red-400 font-bold w-fit text-sm py-1 px-3 rounded-md mt-2">
-                        {errors.password}
-                      </p>
-                    )}
-                  </div>
-                </div>
-                <div>
-                  <button type="submit" className="btn flex w-full justify-center px-3 py-1.5 rounded-lg text-sm font-semibold leading-6 text-slate-900">
-                    Invia
-                  </button>
-                </div>
-              </form>
+            <div>
+              <button type="submit" className="btn flex w-full justify-center px-3 py-1.5 rounded-lg text-sm font-semibold leading-6 text-slate-900">
+                Invia
+              </button>
             </div>
-          </>
-        )}
+          </form>
+        </div>
       </div>
     </section>
   );
